9320913366704736a051b9397004af070930dacd
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-08-09 12:44:41 +0300

    minor changes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,8 @@
 <footer class="border-t border-gray-700 bg-gray-800 py-6 text-center text-white">
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 		<p class="text-sm">
-			Made for <span class="font-semibold text-red-400">Reddit</span> freelancers by
+			Made for <a href="https://www.reddit.com" class="font-semibold text-red-400">Reddit</a>
+			freelancers by
 			<a
 				href="https://loisakitakaya.info"
 				class="font-medium text-blue-400 transition-colors duration-200 hover:text-blue-300"
